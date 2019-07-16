10e9f1c7d301f4e6f8315a26ca7333685cf18d38
matveymaletskov <maletskovitis@mail.ru>
2019-07-16 08:25:20 +0300

    initialize commit

---
+++ src/main/java/ru/itis/maletskov/internship/SimbirSoftInternshipApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.maletskov.internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoftInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbirSoftInternshipApplication.class, args);
+	}
+
+}
